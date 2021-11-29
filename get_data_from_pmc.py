37f393f5dbc996717639568592f27970f9fe0b5e
Xindi Wang <starwxd@gmail.com>
2021-11-29 22:40:16 +0800

    get pmid pmc id pairs

---
+++ get_data_from_pmc.py
@@ -0,0 +1,51 @@
+import argparse
+import ijson
+import json
+import os
+import pickle
+import urllib.request
+import xml.etree.ElementTree as ET
+
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+
+def mapping_pmid2pmcid(filelist):
+    id_pairs = []
+    with open(filelist, 'r') as f:
+        for line in f:
+            info = line.split('\t')
+            if len(info) <= 3:
+                continue
+            else:
+                pmc_id = info[2]
+                pmid = info[3]
+                if pmid.startswith('PMID:'):
+                    pmid = pmid[5:]
+                id_pairs.append((pmc_id, pmid))
+    return id_pairs
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path')
+    parser.add_argument('--pmids')
+    parser.add_argument('--save')
+    args = parser.parse_args()
+
+    id_pairs = mapping_pmid2pmcid(args.path)
+    with open(args.pmids, "rb") as input_file:
+        pmid = pickle.load(input_file)
+
+    new_pairs = []
+    for pair in tqdm(id_pairs):
+        if pair[1] in pmid:
+            new_pairs.append(pair)
+
+    with open(args.save, "wb") as output_file:
+        pickle.dump(new_pairs, output_file)
+
+
+if __name__ == "__main__":
+    main()
