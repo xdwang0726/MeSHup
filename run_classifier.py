b415923089111382eb34973e97a0cb48580f91bc
Xindi Wang <starwxd@gmail.com>
2022-01-03 10:28:05 +0800

    test

--- run_classifier.py
+++ run_classifier.py
@@ -232,8 +232,8 @@ if __name__ == "__main__":
     # }
     NUM_LINES = {
         'all': 957426,
-        'train': 2500,
-        'dev': 300,
+        'train': 250000,
+        'dev': 30000,
         'test': 95769
     }
     print('load and prepare Mesh')
