cbf92f5dbd23394936f926e3159e42c57841a13f
Xindi Wang <starwxd@gmail.com>
2022-01-04 21:39:29 +0800

    debug

--- run_classifier.py
+++ run_classifier.py
@@ -231,8 +231,8 @@ if __name__ == "__main__":
     #     'test': 95769
     # }
     NUM_LINES = {
-        'all': 957426,
-        'train': 100000,
+        'all': 765920,
+        'train': 250000,
         'dev': 30000,
         'test': 95769
     }
@@ -251,7 +251,7 @@ if __name__ == "__main__":
     num_nodes = len(meshIDs)
 
     print('load pre-trained BioWord2Vec')
-    vocab_iterator = _RawTextIterableDataset(NUM_LINES['all'], _create_data_from_csv_vocab(args.full_path))
+    vocab_iterator = _RawTextIterableDataset(NUM_LINES['all'], _create_data_from_csv_vocab(args.train_path))
     cache, name = os.path.split(args.word2vec_path)
     vectors = Vectors(name=name, cache=cache)
     vocab = build_vocab_from_iterator(yield_tokens(vocab_iterator))
