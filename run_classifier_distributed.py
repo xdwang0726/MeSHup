8a7eeffbecd575bd0d0aa968cdf219936c3eb240
Xindi Wang <starwxd@gmail.com>
2022-01-04 15:02:04 +0800

    debug

--- run_classifier_distributed.py
+++ run_classifier_distributed.py
@@ -189,7 +189,7 @@ if __name__ == "__main__":
 
     parser.add_argument('--num_epochs', type=int, default=10)
     parser.add_argument('--batch_sz', type=int, default=16)
-    parser.add_argument('--num_workers', type=int, default=3)
+    parser.add_argument('--num_workers', type=int, default=4)
     parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--momentum', type=float, default=0.9)
     parser.add_argument('--weight_decay', type=float, default=0)
