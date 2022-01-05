efc74f13503e8a57aefd8761dc303c033f815ba6
Xindi Wang <starwxd@gmail.com>
2022-01-05 19:01:38 +0800

    add current position to dataset

--- run_classifier.py
+++ run_classifier.py
@@ -231,9 +231,9 @@ if __name__ == "__main__":
     #     'test': 95769
     # }
     NUM_LINES = {
-        'all': 765920,
-        'train': 100000,
-        'dev': 15000,
+        'all': 76592,
+        'train': 10000,
+        'dev': 1500,
         'test': 95769
     }
     print('load and prepare Mesh')
